0b1241737e6f3e9516bbdb80d06df09784a91c5b
maskwang520 <maskwang@foxmail.com>
2026-03-31 00:24:57 +0800

    top04

--- top04.go
+++ top04.go
@@ -1,13 +1,37 @@
 package main
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
-	p1, p2 := 0, 0
-	for p1 < len(nums1) && p2 < len(nums2) {
-		if nums1[p1] < nums2[p2] {
-			p1++
+	merged := make([]int, len(nums1)+len(nums2))
+	flag := (len(nums1)+len(nums2))%2 == 0
+	i, j := 0, 0
+	count := 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] < nums2[j] {
+			merged[count] = nums1[i]
+			i++
 		} else {
-			p2++
+			merged[count] = nums2[j]
+			j++
 		}
+		count++
 	}
+
+	for i < len(nums1) {
+		merged[count] = nums1[i]
+		count++
+		i++
+	}
+
+	for j < len(nums2) {
+		merged[count] = nums2[j]
+		count++
+		j++
+	}
+
+	if flag {
+		return float64(merged[(len(nums1)+len(nums2))/2]+merged[(len(nums1)+len(nums2))/2-1]) / 2
+	}
+
+	return float64(merged[(len(nums1)+len(nums2))/2])
+
 }
